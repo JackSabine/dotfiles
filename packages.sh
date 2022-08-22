52043869f864fc2549efe1415b140c414acbfa79
Jack Sabine <jack@sabine.ws>
2022-08-22 17:49:57 -0400

    Comment packages

--- packages.sh
+++ packages.sh
@@ -3,21 +3,26 @@
 # Apt installs
 sudo apt update
 sudo apt upgrade
-sudo apt install git
-sudo apt install curl
-sudo apt install tmux
-sudo apt install neovim
-sudo apt install kicad
-sudo apt install exa
-sudo apt install ripgrep
+
+# GNOME addons
 sudo apt install gnome-tweaks
-sudo apt install piper
-sudo apt install discord
-sudo apt install tree
-sudo apt install steam
-sudo apt install bat
 sudo apt install gnome-screenshot
-sudo apt install ckb-next
+
+sudo apt install git              # Best VCS
+sudo apt install curl             # How else would you download things?
+sudo apt install tmux             # Favorite terminal mux
+sudo apt install neovim           # Favorite editor
+sudo apt install kicad            # Schematic/PCB tool
+sudo apt install piper            # Mouse DPI and LED coloring
+sudo apt install tree             # Ubuntu tree
+sudo apt install bat              # `cat` replacement -- binary named `batcat` on Ubuntu
+sudo apt install exa              # `ls` replacement
+sudo apt install ripgrep          # `grep` replacement
+sudo apt install ckb-next         # Corsair K55 LED coloring
+sudo apt install okular           # PDF Viewer
+
+# Standalone apps
+sudo apt install steam
 
 ## Required for vivado
 sudo apt install libncurses5
