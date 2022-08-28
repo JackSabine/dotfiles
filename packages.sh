7961ce34b8a23d4a85e6fb54ebb37a914cbe9042
Jack Sabine <jack@sabine.ws>
2022-08-27 22:53:32 -0400

    Group peripheral apt installs

--- packages.sh
+++ packages.sh
@@ -8,17 +8,17 @@ sudo apt upgrade
 sudo apt install gnome-tweaks
 sudo apt install gnome-screenshot
 
+sudo apt install piper            # Mouse DPI and LED coloring
+sudo apt install ckb-next         # Corsair K55 LED coloring
 sudo apt install git              # Best VCS
 sudo apt install curl             # How else would you download things?
 sudo apt install tmux             # Favorite terminal mux
 sudo apt install neovim           # Favorite editor
 sudo apt install kicad            # Schematic/PCB tool
-sudo apt install piper            # Mouse DPI and LED coloring
 sudo apt install tree             # Ubuntu tree
 sudo apt install bat              # `cat` replacement -- binary named `batcat` on Ubuntu
 sudo apt install exa              # `ls` replacement
 sudo apt install ripgrep          # `grep` replacement
-sudo apt install ckb-next         # Corsair K55 LED coloring
 sudo apt install okular           # PDF Viewer
 
 # Standalone apps
