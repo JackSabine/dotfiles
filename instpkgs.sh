2bbcdda70dd9510be52a7758d7c79648a6f2b151
Jack Sabine <jack@sabine.ws>
2022-09-08 17:17:49 -0400

    Add snap to install script

--- instpkgs.sh
+++ instpkgs.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# Bash unofficial strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+mint_nosnap_file=/etc/apt/preferences.d/nosnap.pref
+if [[ -f ${mint_nosnap_file} ]]; then
+    sudo awk -i inplace '{print "# " $0}' ${mint_nosnap_file}
+fi
+
 sudo add-apt-repository universe
 
 sudo apt update
 sudo apt upgrade
 
-xargs -a ./packages sudo apt install -y
+xargs -a ./apt_packages sudo apt install -y
 
-# Snap installs
-# sudo snap install discord
+sudo snap install $(cat ./snap_packages)
 
 # gsettings set org.gnome.shell.extensions.dash-to-dock isolate-monitors true
 
 # bash -c  "$(wget -qO- https://git.io/vQgMr)"
 # git clone git@github.com:Znuff/consolas-powerline.git ~/Downloads
+
