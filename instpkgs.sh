e0ab207e00c983df1e4ee71a364ffdead1c93f4f
Jack Sabine <jack@sabine.ws>
2022-11-08 16:09:30 -0500

    Add snap install param support in instpkgs.sh

--- instpkgs.sh
+++ instpkgs.sh
@@ -4,7 +4,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-mint_nosnap_file=/etc/apt/preferences.d/nosnap.pref
+mint_nosnap_file="/etc/apt/preferences.d/nosnap.pref"
+snap_pkglist_file="snap_packages"
+apt_pkglist_file="apt_packages"
+
 if [[ -f ${mint_nosnap_file} ]]; then
   sudo sed -e 's/^#*/#/' -i ${mint_nosnap_file}
 fi
@@ -14,14 +17,23 @@ sudo add-apt-repository universe
 sudo apt update
 sudo apt upgrade -y
 
-xargs -a ./apt_packages sudo apt install -y
+xargs -a ${apt_pkglist_file} sudo apt install -y
 
-# Install individually to incur no errors :(
+# Install individually so individual fails won't cancel all installs
 # https://askubuntu.com/questions/1232034/problem-when-trying-to-install-multiple-apps-with-snap
 # https://forum.snapcraft.io/t/trying-to-re-install-multiple-packages-with-snap-install-fails-with-install-refresh-information-results-from-the-store/24859
-for i in $(cat ./snap_packages); do
-  sudo snap install ${i}
+
+IFS=$'\n' readarray -t packages < ${snap_pkglist_file}
+# Unset e so package installs are allowed to fail
+set +e
+for pkg in ${packages[@]}; do
+  # Split on spaces so package names with install parameters can be separated
+  # snap install 'code' '--classic' (works)
+  # snap install 'code --classic' (package not found)
+  IFS=$' \t' pkg_options=(${pkg})
+  sudo snap install ${pkg_options[@]}
 done
+set -e
 
 echo "Done!"
 
